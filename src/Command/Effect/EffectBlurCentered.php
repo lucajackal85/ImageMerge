47c9cb8de5bdf148dfffb0a0b484e984da37e411
Luca Giacalone <luca.giacalone@edimotive.com>
2017-11-10 09:05:18 +0100

    fixed blur center image

--- src/Command/Effect/EffectBlurCentered.php
+++ src/Command/Effect/EffectBlurCentered.php
@@ -8,6 +8,7 @@
 
 namespace Jackal\ImageMerge\Command\Effect;
 
+use Jackal\ImageMerge\Builder\ImageBuilder;
 use Jackal\ImageMerge\Command\AbstractCommand;
 use Jackal\ImageMerge\Command\Options\DimensionCommandOption;
 use Jackal\ImageMerge\Command\Options\DoubleCoordinateColorCommandOption;
@@ -39,44 +40,39 @@ class EffectBlurCentered extends AbstractCommand
         /** @var DimensionCommandOption $options */
         $options = $this->options;
 
+        $builder = ImageBuilder::fromImage($this->image);
+
         $originalWidth = $this->image->getWidth();
         $originalHeight = $this->image->getHeight();
 
         if ($originalHeight > $options->getHeight()) {
-            $this->image->thumbnail(null, $options->getHeight() - 4);
+            $builder->thumbnail(null, $options->getHeight() - 4);
             $originalWidth = $this->image->getWidth();
             $originalHeight = $this->image->getHeight();
         }
 
         if ($originalWidth > $options->getWidth()) {
-            $this->image->thumbnail($options->getWidth() - 4, null);
+            $builder->thumbnail($options->getWidth() - 4, null);
             $originalWidth = $this->image->getWidth();
             $originalHeight = $this->image->getHeight();
         }
 
         $originalImg = $this->saveImage($this->image);
 
-        $this->image->resize($options->getWidth(), $options->getHeight());
-        $this->image->blur(40);
-        $this->image->brightness(-70);
+        $builder->resize($options->getWidth(), $options->getHeight());
+        $builder->blur(40);
+        $builder->brightness(-70);
 
         $x = round(($options->getWidth() - $originalWidth) / 2);
         $y = round(($options->getHeight() - $originalHeight) / 2);
 
         $borderColor = 'FFFFFF';
 
-        $this->image->addCommand(SquareAssetCommand::class,
-            new DoubleCoordinateColorCommandOption(
-                new Coordinate($x - 1, $y - 1),
-                new Coordinate($x + $originalWidth, $y + $originalHeight),
-                0, $borderColor)
-        );
+        $builder->addSquare($x - 1, $y - 1,$x + $originalWidth, $y + $originalHeight,$borderColor);
 
-        $this->image->addCommand(ImageAssetCommand::class,
-            new SingleCoordinateFileObjectCommandOption($originalImg, new Coordinate($x, $y))
-        );
+        $builder->merge(Image::fromFile($originalImg),$x, $y);
 
-        return $this->image;
+        return $builder->getImage();
     }
 
     /**
